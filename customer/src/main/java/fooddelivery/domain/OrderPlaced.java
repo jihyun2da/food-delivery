b792a3926bdaced8f7473fb94da1d76af247eae2
jihyun2da <117624181+jihyun2da@users.noreply.github.com>
2022-11-22 09:34:11 +0900

    commit

--- customer/src/main/java/fooddelivery/domain/OrderPlaced.java
+++ customer/src/main/java/fooddelivery/domain/OrderPlaced.java
@@ -1,13 +1,16 @@
 package fooddelivery.domain;
 
+import fooddelivery.domain.*;
 import fooddelivery.infra.AbstractEvent;
-import lombok.Data;
+import lombok.*;
 import java.util.*;
-
 @Data
+@ToString
 public class OrderPlaced extends AbstractEvent {
 
     private Long id;
     private String foodId;
     private String customerId;
 }
+
+
